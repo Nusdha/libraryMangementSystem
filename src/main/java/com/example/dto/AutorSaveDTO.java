6eb8296c5d2245f3bea58d99554f7bf4040de0e0
nusdhaushad <nusdham-ps20203@stu.kln.ac.lk>
2024-12-08 18:26:37 +0530

    commit

---
+++ src/main/java/com/example/dto/AutorSaveDTO.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class AutorSaveDTO {
+
+    private String author_name;
+}
